87212c3e3a8e5a0513350a09453cc37039dfaab9
charlie1099 <cdcharlesworth@gmail.com>
2022-10-18 21:25:53 -0400

    added more sections

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10">
+      <main className="bg-white  md:px-20 lg:px-10">
         <section className="  min-h-screen">
           <nav className="py-10 md-12 flex justify-between">
             <h1 className="text-xl">Cameron Charlesworth</h1>
@@ -30,7 +30,7 @@ export default function Home() {
               </li>
               <li className="px-5">About Me</li>
               <li className="pr-5">Portfolio</li>
-              <li>Countact</li>
+              <li>Contact</li>
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
@@ -43,11 +43,11 @@ export default function Home() {
           </nav>
 
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Cameron Charlesworth
             </h2>
-            <h3 className="text-2xl py-2">Frontend Devloper</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <h3 className="text-2xl py-2 md:text-3xl">Frontend Devloper</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl">
               happy to see you lets see if we can work to gether
             </p>
           </div>
@@ -61,38 +61,53 @@ export default function Home() {
 
         <section>
           <div>
-            <h3 className="text-3xl py-1">About Me</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
+            <h3 className="text-3xl py-1 md:text-3xl text-center">About Me</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
               Hi, my name is Cameron and I am aiming to become a Junior Software
               Developer. I have recently completed the Utah Coding Bootcamp
-              where I developed <span className="text-teal-500">skills</span> in a variety of developer tools such as
-              JavaScript, Node.js, SQL, Express.js, MVC, OOR, and PWA. I also
-              had the opportunity to be a part of three class projects where we
-              would work with 4 people and had to communicate, deligate the work
-              load and make GitHub issues for the project for the group and
-              pesent the project to the teacher, teacher assistants and cohort.
-              <br />
-              <br />I loved the challenge of learning so much over the six
-              months that I was at the course learing the <span className="text-teal-500">MERN</span> stack. I look
-              forward to learing more as my journey on this exciting creer path
-              has just started.
+              where I developed <span className="text-teal-500">skills</span> in
+              a variety of developer tools such as JavaScript, Node.js, SQL,
+              Express.js, MVC, OOR, and PWA. I also had the opportunity to be a
+              part of three class projects where we would work with 4 people and
+              had to communicate, deligate the work load and make GitHub issues
+              for the project for the group and pesent the project to the
+              teacher, teacher assistants and cohort. I loved the challenge of
+              learning so much over the six months that I was at the course
+              learing the <span className="text-teal-500">MERN</span> stack. I
+              look forward to learing more as my journey on this exciting creer
+              path has just started.
             </p>
           </div>
         </section>
 
         <section>
+          <div className="py-5">
+            <h3 className="text-3xl py-1 text-center">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 text-center">
+              Current finished projects.
+            </p>
+            <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+              <div className="text-center shadow-xl px-2 rounded-xl basis-1/3 flex-1">
+                <Image src={CodingQuiz} width={"100%"} height={"100%"} className="rounded-lg" layout={"responsive"}/>
+              </div>
+              <div className="text-center shadow-xl px-2 rounded-xl basis-1/3 flex-1">
+                <Image src={DayScheduler} width={"100%"} height={"100%"} className="rounded-lg" layout={"responsive"}/>
+              </div>
+              <div className="text-center shadow-xl px-2 rounded-xl basis-1/3 flex-1">
+                <Image src={WeatherAPI} width={"100%"} height={"100%"} className="rounded-lg" layout={"responsive"}/>
+              </div>
+              <div className="text-center shadow-xl px-2 rounded-xl basis-1/3 flex-1">
+                <Image src={EmployeeTracker} width={"100%"} height={"100%"} className="rounded-lg" layout={"responsive"}/>
+              </div>
+              <div className="text-center shadow-xl px-2 rounded-xl basis-1/3 flex-1">
+                <Image src={NodeGeneratedReadeMe} width={"100%"} height={"100%"} className="rounded-lg" layout={"responsive"} />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
           <div>
-            <h3 className="text-3xl py-1">Portfolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">Current finished projects.</p>
-            <div className="flex">
-             <div className="border border-black flex">
-              <Image src={CodingQuiz} width={500} height={500} />
-            </div>
-            <div className="border border-black flex">
-              <Image src={DayScheduler} width={500} height={500}/>
-            </div>
-            </div>
-
+            <h3 className="text-3xl py-1 text-center">Contact</h3>
           </div>
         </section>
       </main>
