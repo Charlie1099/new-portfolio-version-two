1355530b88ee677a80d39be5a491cda99a6135a1
charlie1099 <cdcharlesworth@gmail.com>
2022-10-25 00:13:30 -0400

    added icons and styling

---
+++ portfolio-two/src/Sidebar/index.js
@@ -0,0 +1,69 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../assets/images/logo-s.png'
+import LogoSubtitle from '../assets/images/logo_sub.png'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img classname="sub-logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/cameron-charlesworth-6b457b22a"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/Charlie1099"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="mailto:cdcharlesworth@gmail.com?subject=Contact from Portfolio Site"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
